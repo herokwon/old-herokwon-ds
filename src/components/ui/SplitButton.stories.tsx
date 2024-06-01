085dd792b8adc5afd79509b53f041b6a81053dc1
herokwon <contact@herokwon.dev>
2024-06-01 23:12:04 +0900

    Feat: SplitButton 컴포넌트 추가

---
+++ src/components/ui/SplitButton.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import SplitButton from "./SplitButton";
+
+const dummyItems: React.ComponentProps<typeof SplitButton>['items'] = [
+    ...Array.from({ length: 3 }, (_, i) => ({
+        id: crypto.randomUUID(),
+        heading: `Item ${i + 1}`,
+        description: `This is optional description for item ${i + 1}.`,
+        isDisabled: i === 2,
+        isSelected: false,
+        onClick: fn(),
+    }))];
+
+const meta = {
+    title: 'Components/Buttons/SplitButton',
+    tags: ['autodocs'],
+    component: SplitButton,
+    args: {
+        defaultLabel: 'Default label',
+        size: 'md',
+        spacing: 'default',
+        items: [...dummyItems],
+        isDisabled: false,
+        isSelected: false,
+        isLoading: false,
+    },
+} satisfies Meta<typeof SplitButton>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        variant: 'default',
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+    },
+};
---
+++ src/components/ui/SplitButton.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useMemo, useState } from "react";
+import { FaChevronDown } from "react-icons/fa6";
+
+import type { ButtonProps, ElementBaseVariant, ElementSpacing, ElementStates } from "@/types";
+import TextButton from "./TextButton";
+import IconButton from "./IconButton";
+
+interface SplitButtonItem extends Omit<ElementStates, 'isLoading'> {
+    id: string;
+    heading: string;
+    description?: string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+interface SplitButtonProps extends Omit<ButtonProps, 'spacing' | 'href'> {
+    defaultLabel?: string;
+    variant?: Exclude<ElementBaseVariant, 'secondary'>;
+    spacing?: Exclude<ElementSpacing, 'none'>;
+    items: SplitButtonItem[];
+};
+
+export default function SplitButton({ defaultLabel, variant = 'default', size = 'md', spacing = 'default', items, ...props }: SplitButtonProps) {
+    const { isDisabled = false, isSelected = false, isLoading = false, ...rest } = props;
+
+    const [isShowingItems, setIsShowingItems] = useState<boolean>(isSelected);
+    const [splitButtonItems, setSplitButtonItems] = useState<SplitButtonItem[]>([...items]);
+    const selectedItem = useMemo(() =>
+        splitButtonItems.find((item) =>
+            item.isSelected) ?? null, [splitButtonItems]);
+
+    useEffect(() => {
+        if (selectedItem) setIsShowingItems(false);
+    }, [selectedItem]);
+
+    return (
+        <div className="h-full flex relative group">
+            <TextButton
+                {...rest}
+                label={selectedItem?.heading ?? defaultLabel ?? ''}
+                variant={variant}
+                size={size}
+                spacing={spacing}
+                isDisabled={isDisabled}
+                isSelected={isShowingItems}
+                isLoading={isLoading}
+                title={selectedItem?.description}
+                className='rounded-r-none'
+                onClick={isDisabled ?
+                    undefined :
+                    selectedItem?.onClick} />
+            <div className={`w-1 min-h-full ${isDisabled ?
+                'disabled' :
+                variant === 'primary' ?
+                    'bg-blue-600 dark:bg-blue-500' :
+                    'bg-tertiary'} transition-all`} />
+            <IconButton
+                icon={FaChevronDown}
+                variant={variant}
+                size={size === 'lg' ?
+                    'md' :
+                    size === 'sm' ?
+                        'xs' :
+                        'sm'}
+                spacing={spacing}
+                shape='square'
+                isDisabled={isDisabled}
+                className={`rounded-l-none ${isShowingItems ?
+                    'first:*:rotate-180' :
+                    ''} first:*:transition-all`}
+                onClick={() => !isDisabled &&
+                    setIsShowingItems((prev) => !prev)} />
+            <div className={`p-1 my-2 rounded-ms border border-tertiary bg-light-primary dark:bg-dark-secondary ${isShowingItems ?
+                '' :
+                'pointer-events-none opacity-0 translate-y-[0.25rem]'} transition-all absolute top-full right-0`}>
+                <ul className="w-full flex flex-col gap-1">
+                    {splitButtonItems.map((item) =>
+                        <li
+                            key={item.id}
+                            className={`px-2 py-1 rounded-ms text-sm ${variant === 'primary' ?
+                                (item.isSelected ?
+                                    '!text-dark bg-blue' :
+                                    'hover:!text-dark hover:bg-blue-500 dark:hover:bg-blue-600') :
+                                (item.isSelected ?
+                                    'bg-light-secondary dark:bg-dark-tertiary' :
+                                    'hover:bg-light-secondary dark:hover:bg-dark-tertiary')} cursor-pointer transition-all`}
+                            onClick={() => setSplitButtonItems((prevItems) => [
+                                ...prevItems.map((prevItem) => ({
+                                    ...prevItem,
+                                    isSelected: prevItem.id === item.id,
+                                }))
+                            ])}>
+                            <p className="w-full font-semibold whitespace-pre">
+                                {item.heading}
+                            </p>
+                            {item.description &&
+                                <p className="w-full text-xs opacity-normal whitespace-pre">
+                                    {item.description}
+                                </p>}
+                        </li>)}
+                </ul>
+            </div>
+        </div>
+    );
+}
