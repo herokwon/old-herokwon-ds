eaafd38aaabca5fc0a96d41a57a77b0be9ea9971
herokwon <contact@herokwon.dev>
2024-06-05 14:15:29 +0900

    Feat: Range 컴포넌트 추가

---
+++ src/components/form/Range.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Range from "./Range";
+
+const meta = {
+    title: 'Components/Range',
+    tags: ['autodocs'],
+    component: Range,
+    args: {
+        isShowingLabel: false,
+        labelDirection: 'left',
+        min: 0,
+        max: 100,
+        step: 1,
+    },
+} satisfies Meta<typeof Range>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const ShowingLabel: Story = {
+    args: {
+        isShowingLabel: true,
+    },
+};
+
+export const HelperMessage: Story = {
+    args: {
+        helperMessage: 'This is a helper message',
+    },
+};
+
+export const ErrorMessage: Story = {
+    args: {
+        errorMessage: 'Error!',
+    },
+};
