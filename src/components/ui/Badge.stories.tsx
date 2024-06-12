c8bb4047272af0480fafb116e453e65409587184
herokwon <contact@herokwon.dev>
2024-06-12 14:59:15 +0900

    Feat: Badge 컴포넌트 추가

---
+++ src/components/ui/Badge.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Badge from "./Badge";
+
+const meta = {
+    title: 'Components/Badge',
+    tags: ['autodocs'],
+    component: Badge,
+    args: {
+        value: 100,
+        size: 'sm',
+        shape: 'round',
+        maxValue: undefined,
+    },
+} satisfies Meta<typeof Badge>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        variant: 'default',
+        maxValue: 100,
+    },
+};
+
+export const MaxValue: Story = {
+    args: {
+        variant: 'default',
+        maxValue: 99,
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+        maxValue: 100,
+    },
+};
+
+export const Added: Story = {
+    args: {
+        variant: 'added',
+    },
+};
+
+export const Removed: Story = {
+    args: {
+        variant: 'removed',
+    },
+};
