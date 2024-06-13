0acc6e77ca72cfd35fb27e1b565f8e7657d487fa
herokwon <contact@herokwon.dev>
2024-06-13 18:17:30 +0900

    Design (Tag): 'removable' 스타일 클래스 추가

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { LuHash, LuX } from "react-icons/lu";
 
 import type { ElementBaseSize, ElementSpacing } from "@/types";
@@ -14,10 +14,22 @@ export default function Tag({ isRemovable = false, ...props }: TagProps) {
     const { size = 'sm', spacing = 'compact', iconBefore = { content: LuHash }, iconAfter, ...restProps } = props;
 
     const [isRemoved, setIsRemoved] = useState<boolean>(false);
+    const tagRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        if (!isRemovable || !tagRef.current || !tagRef.current.lastElementChild) return;
+        const removeButton = tagRef.current.lastElementChild as HTMLElement;
+
+        removeButton.onmouseenter = () =>
+            tagRef.current?.classList.add('removable');
+        removeButton.onmouseleave = () =>
+            tagRef.current?.classList.remove('removable');
+    }, [isRemovable]);
 
     return (
         <TextButton
             {...restProps}
+            ref={tagRef}
             isHoverable={false}
             size={size}
             spacing={spacing}
