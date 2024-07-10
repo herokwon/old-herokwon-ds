24dbd68943d398993b75f5b4357ebc1f51863341
herokwon <contact@herokwon.dev>
2024-07-10 23:31:32 +0900

    Refactor (Heading): content 속성 제거

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,30 +1,18 @@
-import type { ElementType, ElementWithHref, PolymorphicElementPropsWithoutRef } from "../../types";
-import LinkWrapper from "../LinkWrapper";
+import type { ElementWithHref, PolymorphicElementPropsWithoutRef } from "../../types";
+import LinkableElement from "../LinkableElement";
 
-type HeadingElement = Extract<ElementType, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
+type HeadingElement = Extract<React.ElementType, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
 
-type _HeadingProps = ElementWithHref & {
-    content: string;
-};
+type HeadingProps<T extends HeadingElement> = PolymorphicElementPropsWithoutRef<T, ElementWithHref>;
 
-type HeadingProps<T extends HeadingElement> = PolymorphicElementPropsWithoutRef<T, _HeadingProps>;
-
-export default function Heading<T extends HeadingElement = 'h1'>({ children, as, href, content, ...props }: HeadingProps<T>) {
+export default function Heading<T extends HeadingElement = 'h1'>({ children, as, href, ...props }: HeadingProps<T>) {
     const Element = as || 'h1';
 
     return (
-        <Element
-            {...props}
-            className={`${props.className ?? ''}`}>
-            <p>
-                <LinkWrapper
-                    href={href?.to}
-                    replace={href?.replace}
-                    className="hover:underline underline-offset-2">
-                    {content}
-                </LinkWrapper>
-            </p>
-            {children}
+        <Element {...props}>
+            <LinkableElement as='p' href={href}>
+                {children}
+            </LinkableElement>
         </Element>
     );
 }
