ab04675a35728dc545c8997750525cde69a0d66d
herokwon <contact@herokwon.dev>
2024-07-20 15:01:57 +0900

    Refactor (Tooltip): 배경 색상 변경 & 말풍선 꼬리 효과 적용 (Popup보다 제한적 역할 + Only 마우스 오버 활성화)

--- src/components/ui/Tooltip.stories.tsx
+++ src/components/ui/Tooltip.stories.tsx
@@ -8,10 +8,8 @@ const meta = {
   tags: ['autodocs'],
   component: Tooltip,
   args: {
-    hasTail: true,
-    size: 'md',
     position: 'bottom-center',
-    triggerItem: <TextButton label="Hover on Button" />,
+    children: <TextButton label="Hover on Button" />,
   },
 } satisfies Meta<typeof Tooltip>;
 export default meta;
@@ -20,13 +18,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    content: 'This is a tooltip with tail',
-  },
-};
-
-export const WithoutTail: Story = {
-  args: {
-    hasTail: false,
-    content: 'This is a tooltip without tail',
+    content: 'This is a tooltip',
   },
 };
