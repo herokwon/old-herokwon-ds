e6fbba341593ba0f2e61a3144fa2b1a2d56804a3
herokwon <contact@herokwon.dev>
2024-07-21 17:19:25 +0900

    Refactor: 누락된 next-env.d.ts 파일 생성 (Next.js 타입 적용) → custom.d.ts 'png' 확장자 참조 타입 제거

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,8 +2,3 @@ declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
