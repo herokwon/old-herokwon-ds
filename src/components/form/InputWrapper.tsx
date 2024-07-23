c1614eb53da3e824d1a2a349a76556bf373b3d4b
herokwon <contact@herokwon.dev>
2024-07-23 12:29:50 +0900

    Design (Input): Focus 적용 범위 확대 (input 태그 → input 포함하는 부모 요소)

--- src/components/form/InputWrapper.tsx
+++ src/components/form/InputWrapper.tsx
@@ -1,4 +1,8 @@
-interface InputWrapperProps extends React.ComponentPropsWithoutRef<'div'> {
+import { useInput } from '../../hooks';
+
+interface InputWrapperProps
+  extends Pick<ReturnType<typeof useInput<HTMLInputElement>>, 'onFocusInput'>,
+    React.ComponentPropsWithoutRef<'div'> {
   children: React.ReactNode;
   isFocused: boolean;
   hasError: boolean;
@@ -8,11 +12,16 @@ export default function InputWrapper({
   children,
   isFocused,
   hasError,
+  onFocusInput,
   ...props
 }: InputWrapperProps) {
   return (
     <div
       {...props}
+      onClick={e => {
+        onFocusInput();
+        props.onClick && props.onClick(e);
+      }}
       className={`flex w-full items-center rounded-ms border ${
         isFocused
           ? hasError
