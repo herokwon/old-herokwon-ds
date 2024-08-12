e4550e5e2d0498398d3f01e466833c3222682fb3
herokwon <contact@herkwon.dev>
2024-08-13 00:50:28 +0900

    Design (Backdrop): overflow 스타일 변경

--- src/components/ui/Backdrop.tsx
+++ src/components/ui/Backdrop.tsx
@@ -14,7 +14,7 @@ export default function Backdrop({
     <>
       <section
         {...restProps}
-        className={`fixed bottom-0 left-0 right-0 top-0 h-screen w-screen overflow-y-auto bg-light-primary/bold transition-opacity duration-200 dark:bg-dark-primary/bold ${isActive ? 'z-50' : 'pointer-events-none -z-50 opacity-0'} ${restProps.className ?? ''}`}
+        className={`fixed bottom-0 left-0 right-0 top-0 h-screen w-screen bg-light-primary/bold transition-opacity duration-200 dark:bg-dark-primary/bold ${isActive ? 'z-50' : 'pointer-events-none -z-50 opacity-0'} ${restProps.className ?? ''}`}
         onClick={e => {
           !e.currentTarget.firstElementChild?.contains(
             e.target as Node | null,
