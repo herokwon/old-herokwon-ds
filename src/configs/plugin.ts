bcba435df45014f776ae03020749e947f6f2ca70
herokwon <contact@herkwon.dev>
2024-08-16 17:20:30 +0900

    Feat (tailwind-plugin): 'max-xs' variant 추가

--- src/configs/plugin.ts
+++ src/configs/plugin.ts
@@ -11,6 +11,7 @@ const customVariants: { [key: string]: string | string[] } = {
   'scrollbar-thumb': ['&::-webkit-scrollbar-thumb'],
   'scrollbar-track': ['&::-webkit-scrollbar-track'],
   xs: '@media (min-width: 512px)',
+  'max-xs': '@media not all and (min-width: 512px)',
 };
 
 export default plugin(function ({
