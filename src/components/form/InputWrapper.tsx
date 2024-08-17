ff7ad05e252c42b4987baae871fead367ac5bfa2
herokwon <contact@herkwon.dev>
2024-08-17 10:51:30 +0900

    Refactor (InputWrapper): 'size' 속성 추가

--- src/components/form/InputWrapper.tsx
+++ src/components/form/InputWrapper.tsx
@@ -1,15 +1,20 @@
+import type { ElementBaseSize } from '../../types';
+
 import { useInput } from '../../hooks';
 
 interface InputWrapperProps
-  extends Pick<ReturnType<typeof useInput<HTMLInputElement>>, 'onFocusInput'>,
+  extends Pick<
+      ReturnType<typeof useInput<HTMLInputElement>>,
+      'isFocused' | 'hasError' | 'onFocusInput'
+    >,
     React.ComponentPropsWithoutRef<'div'> {
   children: React.ReactNode;
-  isFocused: boolean;
-  hasError: boolean;
+  size: ElementBaseSize;
 }
 
 export default function InputWrapper({
   children,
+  size,
   isFocused,
   hasError,
   onFocusInput,
@@ -22,13 +27,18 @@ export default function InputWrapper({
         onFocusInput();
         props.onClick?.(e);
       }}
-      className={`flex w-full items-center rounded-ms border ${
+      className={`flex w-full items-center border-b py-1 ${
         isFocused
           ? hasError
             ? 'border-light-red last:*:caret-light-red dark:border-dark-red dark:last:*:caret-dark-red'
             : 'border-light-blue last:*:caret-light-blue dark:border-dark-blue dark:last:*:caret-dark-blue'
           : 'border-light-tertiary dark:border-dark-tertiary'
-      } cursor-pointer overflow-hidden bg-light-primary transition-all dark:bg-dark-secondary ${props.className ?? ''}`}
+      } ${
+        size === 'lg' ? 'text-lg' : size === 'sm' ? 'text-sm' : 'text-base'
+      } overflow-hidden bg-light-primary transition-all dark:bg-dark-secondary ${props.className ?? ''}`}
+      style={{
+        maxHeight: `calc(${size === 'lg' ? 1.75 : size === 'sm' ? 1.25 : 1.5}rem + (0.25rem * 2) + 1px)`,
+      }}
     >
       {children}
     </div>
