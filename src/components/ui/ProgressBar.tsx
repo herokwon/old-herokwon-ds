982ad71ce09f5ab1fbc26c4f17e619d6dc6832ca
herokwon <contact@herokwon.dev>
2024-08-26 11:52:28 +0900

    Refactor (ProgressBar): progress 태그 활용

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -10,25 +10,26 @@ export default function ProgressBar({
   const { isActive = false, ...restProps } = props;
 
   return (
-    isActive && (
+    isActive &&
+    (percent === 0 ? (
       <div
         {...restProps}
         className={`relative h-5 w-full overflow-x-hidden rounded-full bg-light-secondary *:bg-light-blue dark:bg-dark-secondary *:dark:bg-dark-blue ${restProps.className ?? ''}`}
       >
-        {percent > 0 ? (
-          <span
-            className="absolute left-0 top-0 h-full w-full origin-left rounded-full transition-transform"
-            style={{
-              transform: `scaleX(${percent}%)`,
-            }}
-          />
-        ) : (
-          <>
-            <span className="absolute left-0 top-0 h-full w-[25%] animate-[progress_1.5s_ease_infinite] rounded-full" />
-            <span className="absolute left-0 top-0 h-full w-[25%] animate-[progress_1.5s_ease_infinite_0.08s] rounded-full" />
-          </>
-        )}
+        <span className="absolute left-0 top-0 h-full w-[25%] animate-[progress_1.5s_ease_infinite] rounded-full" />
+        <span className="absolute left-0 top-0 h-full w-[25%] animate-[progress_1.5s_ease_infinite_0.08s] rounded-full" />
       </div>
-    )
+    ) : (
+      <div
+        {...restProps}
+        className={`flex h-fit w-full ${restProps.className ?? ''}`}
+      >
+        <progress
+          value={percent}
+          max={100}
+          className="h-5 w-full appearance-none overflow-x-hidden rounded-full progress-bar:bg-light-secondary progress-value:rounded-full progress-value:bg-light-blue dark:progress-bar:bg-dark-secondary dark:progress-value:bg-dark-blue"
+        />
+      </div>
+    ))
   );
 }
