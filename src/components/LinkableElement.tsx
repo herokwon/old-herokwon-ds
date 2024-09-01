0bf806478826aab5e35e2026ef96650dc13dda2e
herokwon <contact@herokwon.dev>
2024-09-01 21:39:38 +0900

    Refactor (LinkableElement): CSR에서의 useRouter 제한적 함수 사용을 위한 컴포넌트 분리

--- src/components/LinkableElement.tsx
+++ src/components/LinkableElement.tsx
@@ -10,24 +10,46 @@ import Box from './Box';
 type LinkableElementProps<T extends React.ElementType> =
   PolymorphicElementPropsWithoutRef<T, ElementWithHref>;
 
-export default function LinkableElement<T extends React.ElementType>({
+type LinkElementProps<T extends React.ElementType = 'div'> =
+  LinkableElementProps<T> & Required<ElementWithHref>;
+
+const LinkElement = <T extends React.ElementType = 'div'>({
   children,
   as,
   href,
   ...props
-}: LinkableElementProps<T>) {
+}: LinkElementProps<T>) => {
   const { push, replace } = useRouter();
 
   return (
     <Box
       {...props}
-      as={as || 'div'}
-      className={`${href ? 'linkable !transition-[background-size,_color]' : ''} ${props.className ?? ''}`}
+      as={as ?? 'div'}
+      className={`linkable !transition-[background-size,_color] ${props.className ?? ''}`}
       onClick={e => {
         props.onClick?.(e);
         href && (href.replace ? replace(href.to) : push(href.to));
       }}
     >
+      {children}
+    </Box>
+  );
+};
+
+export default function LinkableElement<T extends React.ElementType = 'div'>({
+  children,
+  as,
+  href,
+  ...props
+}: LinkableElementProps<T>) {
+  if (href)
+    return (
+      <LinkElement {...props} as={as ?? 'div'} href={href}>
+        {children}
+      </LinkElement>
+    );
+  return (
+    <Box {...props} as={as ?? 'div'}>
       {children}
     </Box>
   );
